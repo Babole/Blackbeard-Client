dd01c1336f9589b847e2a50fd1167f2d564d336f
Duncan <duncan.cornish.1999@gmail.com>
2022-10-28 17:05:56 +0100

    setup tests, add tests for app, login page, register page

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,11 +1,21 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
 
 describe('App', () => {
   beforeEach(() => {
-      render(<App />)
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
   })
-    
+
+  test('Renders the app', () => {
+    const app = screen.getByRole("application")
+    expect(app).toBeInTheDocument();
+  });
+
   // test('renders learn react link', () => {
   //   const linkElement = screen.getByText(/learn react/i);
   //   expect(linkElement).toBeInTheDocument();
